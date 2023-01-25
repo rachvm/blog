d59945e2df7db5c24df88beab2ca448fa0748e7f
Alistair Hey <alistair@heyal.co.uk>
2023-01-25 17:56:28 +0000

    Add drafts folder, should only render locally
    Signed-off-by: Alistair Hey <alistair@heyal.co.uk>

--- src/lib/getAllArticles.js
+++ src/lib/getAllArticles.js
@@ -7,6 +7,20 @@ async function importArticle(articleFilename) {
   )
   return {
     slug: articleFilename.replace(/(\/index)?\.mdx$/, ''),
+    href: `/articles/${articleFilename.replace(/(\/index)?\.mdx$/, '')}`,
+    ...meta,
+    component,
+  }
+}
+
+
+async function importDraft(articleFilename) {
+  let { meta, default: component } = await import(
+      `../pages/drafts/${articleFilename}`
+      )
+  return {
+    slug: articleFilename.replace(/(\/index)?\.mdx$/, ''),
+    href: `/drafts/${articleFilename.replace(/(\/index)?\.mdx$/, '')}`,
     ...meta,
     component,
   }
@@ -16,9 +30,17 @@ export async function getAllArticles() {
   let articleFilenames = await glob(['*.mdx', '*/index.mdx'], {
     cwd: path.join(process.cwd(), 'src/pages/articles'),
   })
+  let draftFilenames = await glob(['*.mdx', '*/index.mdx'], {
+    cwd: path.join(process.cwd(), 'src/pages/drafts'),
+  })
+  // if env.IS_PRODUCTION is false, then we load an extra folder called "drafts"
+
 
   let articles = await Promise.all(articleFilenames.map(importArticle))
-
+  if (process.env.NODE_ENV !== 'production') {
+    let drafts = await Promise.all(draftFilenames.map(importDraft))
+    articles = [...articles, ...drafts]
+  }
   return articles.sort((a, z) => new Date(z.date) - new Date(a.date))
 }
  
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -20,7 +20,7 @@ import { config } from "../../config";
 function Article({ article }) {
   return (
     <Card as="article">
-      <Card.Title href={`/articles/${article.slug}`}>
+      <Card.Title href={article.href}>
         {article.title}
       </Card.Title>
       <Card.Eyebrow as="time" dateTime={article.date} decorate>
